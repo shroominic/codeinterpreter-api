8f62e66f1cbe0fbf31f2d803811e64378ab540f8
Shroominic <db2002dominic@gmail.com>
2023-07-14 16:01:10 +0200

    fix test

--- examples/plot_sin_wave.py
+++ examples/plot_sin_wave.py
@@ -7,7 +7,20 @@ async def main():
         output = await session.generate_response(user_request)
         
         print(output.content)
-        print(output.files)
+
+        try:
+            from PIL import Image  # type: ignore
+        except ImportError:
+            print("Please install it with `pip install codeinterpreterapi[image_support]` to display images.")
+            exit(1)
+
+        from io import BytesIO
+        file = output.files[0]
+        img_io = BytesIO(file.content)
+        img = Image.open(img_io)
+        
+        # Display the image
+        img.show()
 
 
 if __name__ == "__main__":
