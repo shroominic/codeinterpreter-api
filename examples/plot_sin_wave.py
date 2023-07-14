795fd03cac0af00d3211509d8aa25e026ff1ef72
Shroominic <db2002dominic@gmail.com>
2023-07-14 16:52:29 +0200

    simplify example

--- examples/plot_sin_wave.py
+++ examples/plot_sin_wave.py
@@ -4,24 +4,11 @@ from codeinterpreterapi import CodeInterpreterSession
 async def main():
     async with CodeInterpreterSession() as session:
         user_request = "Plot a sin wave and show it to me."
+        
         output = await session.generate_response(user_request)
         
-        print(output.content)
-
-        try:
-            from PIL import Image  # type: ignore
-        except ImportError:
-            print("Please install it with `pip install codeinterpreterapi[image_support]` to display images.")
-            exit(1)
-
-        from io import BytesIO
         file = output.files[0]
-        img_io = BytesIO(file.content)
-        img = Image.open(img_io)
-        
-        # Display the image
-        img.show()
-
+        file.show_image()
 
 if __name__ == "__main__":
     import asyncio
