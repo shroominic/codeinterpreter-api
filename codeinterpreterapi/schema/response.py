56a63a1cf1feb382c958b41aeffabd4582777b3e
Shroominic <db2002dominic@gmail.com>
2023-07-14 16:47:11 +0200

    response methods

---
+++ codeinterpreterapi/schema/response.py
@@ -0,0 +1,24 @@
+from langchain.schema import HumanMessage, AIMessage  # type: ignore
+from .file import File
+
+
+class UserRequest(HumanMessage):
+    files: list[File] = []
+    
+    def __str__(self):
+        return self.content
+    
+    def __repr__(self):
+        return f"UserRequest(content={self.content}, files={self.files})"
+
+
+class CodeInterpreterResponse(AIMessage):
+    files: list[File] = []
+    # final_code: str = ""  TODO: implement 
+    # final_output: str = ""  TODO: implement
+    
+    def __str__(self):
+        return self.content
+    
+    def __repr__(self):
+        return f"CodeInterpreterResponse(content={self.content}, files={self.files})"
