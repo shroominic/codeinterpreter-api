25a640f9fdf0cc31406d2cca8f6f35d051954844
Shroominic <db2002dominic@gmail.com>
2023-07-14 14:05:07 +0200

    first implementation still wip

--- examples/plot_sin_wave.py
+++ examples/plot_sin_wave.py
@@ -1,1 +1,15 @@
-# TODO: ask the agent to plot a sin wave and check if an image in the output and show it to the user with PIL.show()
+from codeinterpreterapi import CodeInterpreterSession
+
+
+async def main():
+    async with CodeInterpreterSession() as session:
+        user_request = "Plot a sin wave and show it to me."
+        output = await session.generate_response(user_request)
+        
+        print(output.content)
+        print(output.files)
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
