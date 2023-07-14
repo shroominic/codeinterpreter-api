c20593336c0481b7e01e5e1e23d3b41fd5d611af
Shroominic <db2002dominic@gmail.com>
2023-07-14 18:51:07 +0200

    bitcoin chart example

--- examples/show_bitcoin_chart.py
+++ examples/show_bitcoin_chart.py
@@ -1,1 +1,24 @@
-# TODO: ask the agent to plot the bitcoin chart of 2023 YTD (today is {{currentdate}}) - show the output to the user with PIL.show()
+from datetime import datetime
+from codeinterpreterapi import CodeInterpreterSession
+
+
+async def main():
+    async with CodeInterpreterSession() as session:
+        currentdate = datetime.now().strftime("%Y-%m-%d")
+        user_request = f"Plot the bitcoin chart of 2023 YTD (today is {currentdate})"
+        
+        output = await session.generate_response(user_request)
+        
+        file = output.files[0]
+        file.show_image()
+
+
+# The exciting part about this example is
+# that the code interpreter has internet access
+# so it can download the bitcoin chart from yahoo finance
+# and plot it for you
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
