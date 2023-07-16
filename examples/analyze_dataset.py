11d77f801ba562ad63b6e19d4c1adf7259bdcf98
Shroominic <db2002dominic@gmail.com>
2023-07-16 11:20:52 +0200

    👨🏻‍🔬 dataset analysis example

---
+++ examples/analyze_dataset.py
@@ -0,0 +1,29 @@
+from codeinterpreterapi import CodeInterpreterSession
+from codeinterpreterapi.schema import File
+
+
+async def main():
+    # context manager for start/stop of the session
+    async with CodeInterpreterSession() as session:
+        # define the user request
+        user_request = "Analyze this dataset and plot something interesting about it."
+        files = [
+            File.from_path("examples/assets/iris.csv"),
+        ]
+        
+        # generate the response
+        response = await session.generate_response(
+            user_request, files=files
+        )
+
+        # ouput the response (text + image)
+        print("AI: ", response.content)
+        for file in response.files:
+            file.show_image()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    # run the async function
+    asyncio.run(main())
