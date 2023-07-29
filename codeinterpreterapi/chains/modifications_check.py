5192d9abb1c12332bc63f36be03ed0ef20f0bdbd
Shroominic <db2002dominic@gmail.com>
2023-07-29 14:12:58 +0200

    📼 better prompt for filemod-check

--- codeinterpreterapi/chains/modifications_check.py
+++ codeinterpreterapi/chains/modifications_check.py
@@ -3,67 +3,57 @@ from typing import List, Optional
 
 from langchain.base_language import BaseLanguageModel
 from langchain.chat_models.openai import ChatOpenAI
+from langchain.chat_models.anthropic import ChatAnthropic
 from langchain.schema import AIMessage, OutputParserException
 
-from codeinterpreterapi.prompts import determine_modifications_function, determine_modifications_prompt
+from codeinterpreterapi.prompts import determine_modifications_prompt
 
-# openai functions based
+
 async def get_file_modifications(
-    code: str,
-    llm: ChatOpenAI,
-    retry: int = 2,
-) -> Optional[List[str]]:
-    if retry < 1:
-        return None
-    messages = determine_modifications_prompt.format_prompt(code=code).to_messages()
-    message = await llm.apredict_messages(messages, functions=[determine_modifications_function])
-
-    if not isinstance(message, AIMessage):
-        raise OutputParserException("Expected an AIMessage")
-
-    function_call = message.additional_kwargs.get("function_call", None)
-
-    if function_call is None:
-        return await get_file_modifications(code, llm, retry=retry - 1)
-    else:
-        function_call = json.loads(function_call["arguments"])
-        return function_call["modifications"]
-
-
-async def get_file_modifications_normal(
     code: str,
     llm: BaseLanguageModel,
     retry: int = 2,
 ) -> Optional[List[str]]:
     if retry < 1:
         return None
-    return None
+
+    prompt = determine_modifications_prompt.format(code=code)
+
+    result = await llm.apredict(prompt, stop="```")
+
+    
+    try:
+        result = json.loads(result)
+    except json.JSONDecodeError:
+        result = ""
+    if not result or not isinstance(result, dict) or "modifications" not in result:
+        return await get_file_modifications(code, llm, retry=retry - 1)
+    return result["modifications"]
     
 
-
 async def test():
-    llm = ChatOpenAI(model="gpt-3.5")  # type: ignore
+    llm = ChatAnthropic(model="claude-1.3")  # type: ignore
+    
+    code = \
+        """
+        import matplotlib.pyplot as plt
 
-    code = """
-    import matplotlib.pyplot as plt
+        x = list(range(1, 11))
+        y = [29, 39, 23, 32, 4, 43, 43, 23, 43, 77]
 
-    x = list(range(1, 11))
-    y = [29, 39, 23, 32, 4, 43, 43, 23, 43, 77]
+        plt.plot(x, y, marker='o')
+        plt.xlabel('Index')
+        plt.ylabel('Value')
+        plt.title('Data Plot')
 
-    plt.plot(x, y, marker='o')
-    plt.xlabel('Index')
-    plt.ylabel('Value')
-    plt.title('Data Plot')
-
-    plt.show()
-    """
-
+        plt.show()
+        """
+    
     print(await get_file_modifications(code, llm))
-
+    
 
 if __name__ == "__main__":
-    import asyncio
-    from dotenv import load_dotenv
-    load_dotenv()
+    import asyncio, dotenv
+    dotenv.load_dotenv()
 
     asyncio.run(test())
