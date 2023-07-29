1bca586f091a4eb3aa83370af4e054295682c349
Shroominic <db2002dominic@gmail.com>
2023-07-29 14:13:36 +0200

    🧼 improve prompt

--- codeinterpreterapi/prompts/modifications_check.py
+++ codeinterpreterapi/prompts/modifications_check.py
@@ -1,35 +1,56 @@
-
-from langchain.schema import SystemMessage
-from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import PromptTemplate
 
 
-determine_modifications_prompt = ChatPromptTemplate(
+determine_modifications_prompt = PromptTemplate(
     input_variables=["code"],
-    messages=[
-        SystemMessage(
-            content="The user will input some code and you will need to determine if the code makes any changes to the file system. \n"
-            "With changes it means creating new files or modifying exsisting ones.\n"
-            "Answer with a function call `determine_modifications` and list them inside.\n"
-            "If the code does not make any changes to the file system, still answer with the function call but return an empty list.\n",
-        ),
-        HumanMessagePromptTemplate.from_template("{code}"),
-    ],
+    template=
+        "The user will input some code and you need to determine if the code makes any changes to the file system. \n"
+        "With changes it means creating new files or modifying exsisting ones.\n"
+        "Format your answer as JSON inside a codeblock with a list of filenames that are modified by the code.\n"
+        "If the code does not make any changes to the file system, return an empty list.\n\n"
+        "Determine modifications:\n"
+        "```python\n"
+        "import matplotlib.pyplot as plt\n"
+        "import numpy as np\n\n"
+        "t = np.arange(0.0, 4.0*np.pi, 0.1)\n\n"
+        "s = np.sin(t)\n\n"
+        "fig, ax = plt.subplots()\n\n"
+        "ax.plot(t, s)\n\n"
+        "ax.set(xlabel=\"time (s)\", ylabel=\"sin(t)\",\n"
+        "   title=\"Simple Sin Wave\")\n"
+        "ax.grid()\n\n"
+        "plt.savefig(\"sin_wave.png\")\n"
+        "```\n\n"
+        "Answer:\n"
+        "```json\n"
+        "{{\n"
+        "  \"modifications\": [\"sin_wave.png\"]\n"
+        "}}\n"
+        "```\n\n"
+        "Determine modifications:\n"
+        "```python\n"
+        "import matplotlib.pyplot as plt\n"
+        "import numpy as np\n\n"
+        "x = np.linspace(0, 10, 100)\n"
+        "y = x**2\n\n"
+        "plt.figure(figsize=(8, 6))\n"
+        "plt.plot(x, y)\n"
+        "plt.title(\"Simple Quadratic Function\")\n"
+        "plt.xlabel(\"x\")\n"
+        "plt.ylabel(\"y = x^2\")\n"
+        "plt.grid(True)\n"
+        "plt.show()\n"
+        "```\n\n"
+        "Answer:\n"
+        "```json\n"
+        "{{\n"
+        "  \"modifications\": []\n"
+        "}}\n"
+        "```\n\n"
+        "Determine modifications:\n"
+        "```python\n"
+        "{code}\n"
+        "```\n\n"
+        "Answer:\n"
+        "```json\n",
 )
-
-
-determine_modifications_function = {
-    "name": "determine_modifications",
-    "description": "Based on code of the user determine if the code makes any changes to the file system. \n"
-    "With changes it means creating new files or modifying exsisting ones.\n",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "modifications": {
-                "type": "array",
-                "items": {"type": "string"},
-                "description": "The filenames that are modified by the code.",
-            },
-        },
-        "required": ["modifications"],
-    },
-}
