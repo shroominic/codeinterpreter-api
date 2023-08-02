45bc6bed0c3c5d5d82ab201fdb56be58bfa21366
Shroominic <db2002dominic@gmail.com>
2023-08-02 15:48:43 +0200

    🅰️ claude example

--- examples/anthropic_claude.py
+++ examples/anthropic_claude.py
@@ -4,18 +4,11 @@ from codeinterpreterapi import CodeInterpreterSession
 
 
 async def run_with_claude():
-    llm = ChatAnthropic(model="claude-2")
-    
-    async with CodeInterpreterSession(llm=llm) as session:
+    async with CodeInterpreterSession(model="claude-2") as session:
         result = await session.generate_response(
             "Plot the nvidea stock vs microsoft stock over the last 6 months."
         )
         result.show()
-        
-        result = await session.generate_response(
-            "Store this data to a csv file."
-        )
-        print(result.files)
 
 
 if __name__ == "__main__":
