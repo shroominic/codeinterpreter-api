df8ef240f9d727e7a729c593ebbb84304461be9d
Warren <wwwc@outlook.com>
2023-08-04 17:26:17 +0800

    commit mistake previously

--- codeinterpreterapi/session.py
+++ codeinterpreterapi/session.py
@@ -72,50 +72,48 @@ class CodeInterpreterSession:
             ),
         ]
 
-    class CodeInterpreterSession:
-        # ...
-        def _choose_llm(
-            self,
-            model: str = "gpt-4",
-            openai_api_key: Optional[str] = None,
-            **kwargs
-        ) -> BaseChatModel:
-            if "gpt" in model:
-                openai_api_key = (
-                    openai_api_key 
-                    or settings.OPENAI_API_KEY 
-                    or getenv("OPENAI_API_KEY", None)
+    def _choose_llm(
+        self,
+        model: str = "gpt-4",
+        openai_api_key: Optional[str] = None,
+        **kwargs
+    ) -> BaseChatModel:
+        if "gpt" in model:
+            openai_api_key = (
+                openai_api_key 
+                or settings.OPENAI_API_KEY 
+                or getenv("OPENAI_API_KEY", None)
+            )
+            if openai_api_key is None:
+                raise ValueError(
+                    "OpenAI API key missing. Set OPENAI_API_KEY env variable or pass `openai_api_key` to session."
                 )
-                if openai_api_key is None:
-                    raise ValueError(
-                        "OpenAI API key missing. Set OPENAI_API_KEY env variable or pass `openai_api_key` to session."
-                    )
-                openai_api_version = getenv("OPENAI_API_VERSION")
-                openai_api_base = getenv("OPENAI_API_BASE")
-                deployment_name = getenv("DEPLOYMENT_NAME")
-                openapi_type = getenv("OPENAI_API_TYPE")
-                if openapi_type == "azure" and openai_api_version and openai_api_base and deployment_name:
-                    return AzureChatOpenAI(
-                        temperature=0.03,
-                        openai_api_base=openai_api_base,
-                        openai_api_version=openai_api_version,
-                        deployment_name=deployment_name,
-                        openai_api_key=openai_api_key,
-                        max_retries=3,
-                        request_timeout=60*3
-                    )
-                else:
-                    return ChatOpenAI(
-                        temperature=0.03,
-                        model=model,
-                        openai_api_key=openai_api_key,
-                        max_retries=3,
-                        request_timeout=60 * 3,
-                    )
-            elif "claude" in model:
-                return ChatAnthropic(model=model)
+            openai_api_version = getenv("OPENAI_API_VERSION")
+            openai_api_base = getenv("OPENAI_API_BASE")
+            deployment_name = getenv("DEPLOYMENT_NAME")
+            openapi_type = getenv("OPENAI_API_TYPE")
+            if openapi_type == "azure" and openai_api_version and openai_api_base and deployment_name:
+                return AzureChatOpenAI(
+                    temperature=0.03,
+                    openai_api_base=openai_api_base,
+                    openai_api_version=openai_api_version,
+                    deployment_name=deployment_name,
+                    openai_api_key=openai_api_key,
+                    max_retries=3,
+                    request_timeout=60*3
+                )
             else:
-                raise ValueError(f"Unknown model: {model} (expected gpt or claude model)")
+                return ChatOpenAI(
+                    temperature=0.03,
+                    model=model,
+                    openai_api_key=openai_api_key,
+                    max_retries=3,
+                    request_timeout=60 * 3,
+                )
+        elif "claude" in model:
+            return ChatAnthropic(model=model)
+        else:
+            raise ValueError(f"Unknown model: {model} (expected gpt or claude model)")
 
     def _choose_agent(self) -> BaseSingleActionAgent:
         return (
