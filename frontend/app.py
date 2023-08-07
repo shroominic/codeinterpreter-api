4b9124f1e59f11bdfea10a108ae95b02acd65a58
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2023-08-07 12:51:25 +0000

    pre-commit cleaning

--- frontend/app.py
+++ frontend/app.py
@@ -8,23 +8,26 @@ from datetime import datetime
 
 
 # App title
-st.set_page_config(page_title="Code Interpreter API 🚀",
-                layout="wide")
+st.set_page_config(page_title="Code Interpreter API 🚀", layout="wide")
 # Replicate Credentials
 with st.sidebar:
-    st.title('Code Interpreter API 🚀')
+    st.title("Code Interpreter API 🚀")
     if settings.OPENAI_API_KEY is not None:
-        st.success('OpenAI API key already provided!', icon='✅')
+        st.success("OpenAI API key already provided!", icon="✅")
     else:
-        openai_api = st.text_input('Enter your OpenAI API token:', type='password')
-        if not openai_api.startswith('sk'):
-            st.warning('Please enter your credentials!', icon='⚠️')
+        openai_api = st.text_input("Enter your OpenAI API token:",
+                                   type="password")
+        if not openai_api.startswith("sk"):
+            st.warning("Please enter your credentials!", icon="⚠️")
         else:
             settings.OPENAI_API_KEY = openai_api
-            st.success('Proceed to entering your prompt message!', icon='👉')
+            st.success("Proceed to entering your prompt message!", icon="👉")
             st.experimental_rerun()
 
-    st.markdown('📖 Learn more [here](https://github.com/shroominic/codeinterpreter-api)')
+    st.markdown(
+        "📖 Learn more "
+        "[here](https://github.com/shroominic/codeinterpreter-api)"
+    )
 
 if settings.OPENAI_API_KEY is not None:
     # Initialize database and chat manager
@@ -32,13 +35,13 @@ if settings.OPENAI_API_KEY is not None:
     ci = CodeInterpreter(db)
 
     # Initialize session states
-    if 'current_chat' not in st.session_state:
+    if "current_chat" not in st.session_state:
         st.session_state.current_chat = None
 
-    if 'chats' not in st.session_state:
+    if "chats" not in st.session_state:
         st.session_state.chats = db.get_chats()
 
-    if 'chat_messages' not in st.session_state:
+    if "chat_messages" not in st.session_state:
         st.session_state.chat_messages = []
 
     # Streamlit UI layout
@@ -48,14 +51,18 @@ if settings.OPENAI_API_KEY is not None:
             chat_id = db.save_chat(chat_title)
             st.session_state.current_chat = db.get_chat(chat_id)
             st.session_state.chats = db.get_chats()
-        st.session_state.current_chat = st.radio("Chat Histories", st.session_state.chats, format_func=lambda x: x[1])
+        st.session_state.current_chat = st.radio(
+            "Chat Histories", st.session_state.chats,
+            format_func=lambda x: x[1]
+        )
 
     header_container = st.container()
     chat_container = st.container()
     form_container = st.container()
 
     if st.session_state.current_chat is None:
-        st.caption("Please select a chat history or press 'New Chat' from the sidebar")
+        st.caption("Please select a chat history or press "
+                   "'New Chat' from the sidebar")
     else:
         chat_id = st.session_state.current_chat[0]
         chat_title = st.session_state.current_chat[1]
@@ -73,7 +80,7 @@ if settings.OPENAI_API_KEY is not None:
                 chat_message_id = chat_message[0]
                 category = chat_message[2]
                 content = chat_message[3]
-                if category == 'user':
+                if category == "user":
                     with st.chat_message("user"):
                         st.write(content)
                 else:
@@ -85,22 +92,37 @@ if settings.OPENAI_API_KEY is not None:
                             data = file[3]
                             file_name = file[2]
                             st.image(data)
-                            st.download_button(label=f"Download: {file_name}", data=data, file_name=file_name)
+                            st.download_button(
+                                label=f"Download: {file_name}",
+                                data=data,
+                                file_name=file_name,
+                            )
 
         with st.form(key="user_input", clear_on_submit=True):
-            uploaded_files = st.file_uploader("Choose files for analysis:", accept_multiple_files=True)
-            text_area = st.text_area("Enter your message:", placeholder="Enter your message", value="")
+            uploaded_files = st.file_uploader(
+                "Choose files for analysis:", accept_multiple_files=True
+            )
+            text_area = st.text_area(
+                "Enter your message:",
+                placeholder="Enter your message", value=""
+            )
             if st.form_submit_button("Submit"):
                 user_message = text_area
                 message_id = db.save_message(chat_id, "user", user_message)
-                st.session_state.chat_messages.append(db.get_chat_message(message_id))
+                st.session_state.chat_messages.append(
+                    db.get_chat_message(message_id)
+                    )
                 with chat_container:
                     st.chat_message("user").write(user_message)
 
                 try:
-                    response = asyncio.run(ci.process(user_message, uploaded_files))
-                    message_id = db.save_message(chat_id, "assistant", response.content)
-                    st.session_state.chat_messages.append(db.get_chat_message(message_id))
+                    response = asyncio.run(ci.process(user_message,
+                                                      uploaded_files))
+                    message_id = db.save_message(chat_id, "assistant",
+                                                 response.content)
+                    st.session_state.chat_messages.append(
+                        db.get_chat_message(message_id)
+                    )
                     with chat_container:
                         st.chat_message("assistant").write(response.content)
                         for file in response.files:
@@ -108,9 +130,14 @@ if settings.OPENAI_API_KEY is not None:
                             file_name = file.name
                             db.save_file(message_id, file_name, data)
                             st.image(data, caption=user_message)
-                            st.download_button(label=f"Download: {file_name}", data=data, file_name=file_name)
-                        
+                            st.download_button(
+                                label=f"Download: {file_name}",
+                                data=data,
+                                file_name=file_name,
+                            )
+
                 except Exception as e:
                     with chat_container:
-                        st.write(f"An error occurred: {e.__class__.__name__}: {e}")
+                        st.write(f"An error occurred: "
+                                 f"{e.__class__.__name__}: {e}")
                         st.write(traceback.format_exc())
--- frontend/code_interpreter.py
+++ frontend/code_interpreter.py
@@ -9,15 +9,10 @@ class CodeInterpreter:
     async def process(self, prompt, uploaded_files):
         files = []
         for uploaded_file in uploaded_files:
-            file = File(
-                name=uploaded_file.name,
-                content=uploaded_file.read()
-            )
+            file = File(name=uploaded_file.name, content=uploaded_file.read())
             files.append(file)
-        async with CodeInterpreterSession(model='gpt-3.5-turbo') as session:
+        async with CodeInterpreterSession(model="gpt-3.5-turbo") as session:
             response = await session.generate_response(
-                prompt,
-                files=files,
-                detailed_error=True
+                prompt, files=files, detailed_error=True
             )
             return response
