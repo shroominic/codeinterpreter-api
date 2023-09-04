955c06d08e36678312fbb64dc776fccb27534a44
Shroominic <db2002dominic@gmail.com>
2023-09-04 18:06:04 +0200

    🧼 cleanup example usage

--- examples/anthropic_claude.py
+++ examples/anthropic_claude.py
@@ -1,15 +1,7 @@
-import asyncio
-
 from codeinterpreterapi import CodeInterpreterSession
 
-
-async def run_with_claude():
-    async with CodeInterpreterSession(model="claude-2") as session:
-        result = await session.generate_response(
-            "Plot the nvidea stock vs microsoft stock over the last 6 months."
-        )
-        result.show()
-
-
-if __name__ == "__main__":
-    asyncio.run(run_with_claude())
+with CodeInterpreterSession(model="claude-2") as session:
+    result = session.generate_response(
+        "Plot the nvidea stock vs microsoft stock over the last 6 months."
+    )
+    result.show()
