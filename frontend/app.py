48b5ddeefbf38f0a94382b2620b27b2741e55d99
Shroominic <db2002dominic@gmail.com>
2023-09-29 13:06:57 +0200

    🛠️ fix isort

--- frontend/app.py
+++ frontend/app.py
@@ -2,9 +2,9 @@ import asyncio
 import sys
 
 import streamlit as st
+from utils import get_images  # type: ignore
 
 from codeinterpreterapi import File
-from utils import get_images
 
 # Page configuration
 st.set_page_config(layout="wide")
